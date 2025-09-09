48475126b8b31e758a164f605eb2ddb660fb96a2
gancuuu <kamilganc2009@gmail.com>
2025-09-09 08:58:40 +0200

    Klasa Uczen

--- src/asasa/Uczen.java
+++ src/asasa/Uczen.java
@@ -1,5 +1,37 @@
 package asasa;
 
-public class Uczen {
+public class Uczen extends OSoba {
+    private static int liczbaUczniow = 0;
+    private int nrUcznia;
 
+    public Uczen(String nazwisko, String imie) {
+        super(nazwisko, imie);
+        liczbaUczniow++;
+        nrUcznia=liczbaUczniow;
+    }
+
+    public int getNrUcznia() {
+        return nrUcznia;
+    }
+
+    public void setNrUcznia(int nrUcznia) {
+        this.nrUcznia = nrUcznia;
+    }
+
+    public static int getLiczbaUczniow() {
+        return liczbaUczniow;
+    }
+
+    public static void setLiczbaUczniow(int liczbaUczniow) {
+        Uczen.liczbaUczniow = liczbaUczniow;
+    }
+
+    @Override
+    public String toString() {
+        return "Uczen{" +
+                "nrUcznia=" + nrUcznia +
+                "Imie=" + getImie() +
+                "Nazwisko=" + getNazwisko() +
+                '}';
+    }
 }
