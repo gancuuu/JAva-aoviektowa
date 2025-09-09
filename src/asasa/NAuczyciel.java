22d1f2481773ddf20bba0df53dd88d8c139becd7
gancuuu <kamilganc2009@gmail.com>
2025-09-09 09:13:33 +0200

    Klasa nauczyciel

--- src/asasa/NAuczyciel.java
+++ src/asasa/NAuczyciel.java
@@ -1,5 +1,19 @@
 package asasa;
 
-public class NAuczyciel {
+public class NAuczyciel extends OSoba {
+    private String przedmiot;
 
+    public NAuczyciel(String nazwisko, String imie,String przedmiot) {
+        super(nazwisko, imie);
+        this.przedmiot=przedmiot;
+    }
+
+    @Override
+    public String toString() {
+        return "NAuczyciel{" +
+                "Imie='" + getImie() + '\'' +
+                "NAzwisko='" + getNazwisko() + '\'' +
+                "przedmiot='" + przedmiot + '\'' +
+                '}';
+    }
 }
--- src/asasa/Start.java
+++ src/asasa/Start.java
@@ -1,5 +1,15 @@
 package asasa;
 
 public class Start {
-
+    public static void main(String[] args) {
+        Uczen uczen = new Uczen("Lizak","Maciek");
+        System.out.println(uczen);
+        System.out.println(Uczen.getLiczbaUczniow());
+        Uczen uczen2 = new Uczen("WAli","Mural");
+        System.out.println(uczen2);
+        System.out.println(Uczen.getLiczbaUczniow());
+        Uczen uczen3 = new Uczen("Szatyn","Senpai");
+        System.out.println(uczen3);
+        System.out.println(Uczen.getLiczbaUczniow());
+    }
 }
